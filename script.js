853f2e84b46f12b24583406c886b090859efa9eb
pippo123-pixel <napi2066@gmail.com>
2024-12-22 17:27:08 +0100

    Aggiunta countdown e condizione natale

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let giorniSettimana = new Array("Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato");
 let NUM_SETT = 5;
 let GIORNI_SETT = giorniSettimana.length;
-let intervalID = setInterval(segnaX, 1000);
+let canzone = new Audio("music/FortniteFestiveMusic.mp3");
+
+function playAudio(){
+    canzone.loop = true;
+    canzone.play();
+}
 
 function creaTabella(){
     let stringaTabella = "<table>";
@@ -33,7 +38,36 @@ function creaTabella(){
 
     document.getElementById("box-tabella").innerHTML = stringaTabella;
 
-    segnaX();
+    let mNow = segnaX();
+    if(mNow >= (new Date("2024/12/25 00:00:00").getTime())){
+        // console.log("ciao");
+        let left = 20;
+        let pacchi = new Array();
+        for(let i=0; i < 3; i++){
+            let pacco = document.createElement("img");
+            pacco.src = "img/pacco_"+ i +".png";
+            pacco.width = 100;
+            pacco.style.position = "absolute";
+            pacco.style.left = left + "%";
+            pacco.style.display = "none";
+            pacco.animate([
+                // key frames
+                { top: "0",
+                    display: "block"
+                },
+                { top: "80%",
+                    display: "block"
+                }
+              ], {  
+                // sync options
+                duration: Math.random()*2000 + 1000,
+                iterations: 1
+              });
+            left += 25;
+            document.body.appendChild(pacco);
+        }
+    }
+    let intervalID = setInterval(segnaX, 1000);
 }
 
 function segnaX(){
@@ -57,7 +91,8 @@ function segnaX(){
     let min = Math.floor((secs % 3600) / 60);
     let sec = Math.floor(secs % 60);
 
-    // document.getElementById("countdown").innerHTML = hrs + ":" + min + ":" + sec;
-    // console.log("Ore: " + hrs + " Minuti: " + min + " Secondi: " + sec);
+    document.getElementById("countdown").innerHTML = hrs + " : " + min + " : " + sec;
+    console.log("Ore: " + hrs + " Minuti: " + min + " Secondi: " + sec);
 
+    return millisecsNow;
 }
